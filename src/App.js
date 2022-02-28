7d1a0b1a322ab8e9256ec3011635e5920c10769e
Harsh Vaghani <harshvaghani98@gmail.com>
2022-02-28 14:23:01 +0530

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import ListItems from "./components/ListItems";
 function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    const data = [
+        {
+            id: 0,
+            originalPrice: 450,
+            discountedPrice: 340,
+            thumbnail: "placeholder.png",
+        },
+        {
+            id: 1,
+            originalPrice: 120,
+            discountedPrice: 80,
+            thumbnail: "placeholder.png",
+        },
+        {
+            id: 2,
+            originalPrice: 70,
+            discountedPrice: 45,
+            thumbnail: "placeholder.png",
+        },
+    ];
+
+    return (
+        <div>
+          <ListItems data = {data[0]} />
+          <ListItems data = {data[1]} />
+          <ListItems data = {data[2]} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ListItems.js
@@ -0,0 +1,29 @@
+import AddToCartIcon from "../assets/icons/add_cart.svg";
+const ListItems = ({ data }) => {
+    return (
+        <div className={"item-card"}>
+            <img
+                className={"img-fluid"}
+                src={`assets/${data.thumbnail}`}
+                alt="Item thumbnail"
+            />
+            <div className={"item-card__information"}>
+                <div className={"pricing"}>
+                    <span>₹{data.discountedPrice}</span>
+                    <small>
+                        <strike>₹{data.originalPrice}</strike>
+                    </small>
+                </div>
+                <div className={"title"}>
+                    <h3>Title of the Item!</h3>
+                </div>
+            </div>
+            <button className={"cart-add"}>
+                <span>Add to Cart!</span>
+                <img src={AddToCartIcon} alt="Cart Icon" />
+            </button>
+        </div>
+    );
+};
+
+export default ListItems;
