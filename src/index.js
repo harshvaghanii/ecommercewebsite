7fc57f14ea4cc2c36186bebeb11054b3e262ed0a
Harsh Vaghani <harshvaghani98@gmail.com>
2022-03-26 19:09:14 +0530

    creating a redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
+import store from "./store/index";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById("root")
 );
 
